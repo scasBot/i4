a2554e9c5a6613fe90ea47fabc12338e1ddcafed
wxiao <wxiao@college.harvard.edu>
2013-06-20 22:01:19 -0400

    new files

---
+++ includes/constants.php
@@ -0,0 +1,24 @@
+<?php
+
+    /***********************************************************************
+     * constants.php
+     *
+     * Computer Science 50
+     * Problem Set 7
+     *
+     * Global constants.
+     **********************************************************************/
+
+    // your database's name
+    define("DATABASE", "scas");
+
+    // your database's password
+    define("PASSWORD", "pantsonfire");
+
+    // your database's server
+    define("SERVER", "mysql.hcs.harvard.edu");
+
+    // your database's username
+    define("USERNAME", "scas");
+
+?>
