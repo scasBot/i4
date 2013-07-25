03272f58ea50a5fde1cc25e590ad1e8a66f086d6
hansen <thunderbladeinferno@gmail.com>
2013-07-25 00:52:52 -0400

    added email form

--- html/emailed.php
+++ html/emailed.php
@@ -5,6 +5,7 @@
 	if($_SERVER["REQUEST_METHOD"] == "GET") {
 		if(isset($_GET["SECRET"])) {
 			//var_dump($GLOBALS); 
+			render("email_form.php", array("title" => "Email"));
 		}
 		die(); 
 	} else if ($_SERVER["REQUEST_METHOD"] == "POST") {
