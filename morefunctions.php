30917de7058c3ca51bfa7bf6e20cd3f660ea58a9
wxiao <wxiao@college.harvard.edu>
2013-07-30 13:05:37 -0400

    put in testing

--- morefunctions.php
+++ morefunctions.php
@@ -1,4 +1,12 @@
 <?php
+	function assert2($predicate, $description = "Assert Failure") {
+		if(!$predicate) {
+			throw new Exception($description); 
+		} else {
+			echo "Assert passed!"; 
+		}
+	}
+
     //This is a function that takes a string of emails and put it into one array
 	
 	function parseEmails($string) {
@@ -9,10 +17,49 @@
 		foreach($mailarray as $email){
 			if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
 				array_push($goodmails, $email);
+			} else {
+				throw new Exception("Invalid email: " . $email); 
 			}
 		}
 		return $goodmails;
 	}
-	//parseEmails($string);
+	
+	$passing_strings = array(
+		"willy@chenxiao.us", 
+		"willy@chenxiao.us, tom@bob.com",
+		"willy@chenxiao.us,      tom@bob.com", // multiple spaces 		
+		"<willy xiao> willy@chenxiao.us, <tom bob> tom@bob.com", 
+		"willy@chenxiao.us, <tom bob> tom@bob.com", 
+		"wxiao@college.harvard.edu", 
+		"wxiao.debate@gmail.com", 
+		"willy@chenxiao.us, bob@tom.com, tom@bob.com", 
+		"willy@chenxiao.us, willy@chenxiao.us", 
+	); 
+	$failing_strings = array(
+		"willy", 
+		"google.com", 
+		"@google.com", 
+		"willy@", 
+		"willy@chenxiao.us tom@bob.com", 
+		"willy@chenxiao.us <willy xiao> tom@bob.com", 
+		"willy@chenxiao.us, <willy xiao>, tom@bob.com", 
+		"willy@chenxiao.us, tom@bob.com, ble"
+	); 
+	
+	foreach($passing_strings as $str) {
+		echo "<p>".$str; 
+		var_dump(parseEmails($str)); 
+		echo "</p>"; 
+	}
+	foreach($failing_strings as $str) {
+		try {
+			echo "<p>Failed: "; 
+			var_dump(parseEmails($str));
+			echo "</p>"; 
+		} catch (Exception $e) {
+			echo "<p>Successfully thrown error on : " . $str . "</p>"; 
+		}
+	}
+	
 ?>
 
