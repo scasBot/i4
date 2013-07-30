801836b819d5fcad30ed386602a9f86b5d293cd2
hansen <thunderbladeinferno@gmail.com>
2013-07-30 12:05:19 -0400

    final touch on e-mail form and improved parseEmail function

--- templates/email_form.php
+++ templates/email_form.php
@@ -7,13 +7,13 @@
 			<div class="control-group">
 				<label class="control-label">First Name: *</label>
 				<div class="controls">
-				<input name="firstname" type="text" class="input-large" placeholder="Hingle">
+				<input name="firstname" type="text" class="input-xlarge">
 				</div>
 			</div>
 			<div class="control-group">
 				<label class="control-label">Last Name: *</label>
 				<div class="controls">
-					<input name="lastname" type="text" class="input-large" placeholder="McCringleberry">
+					<input name="lastname" type="text" class="input-xlarge">
 				</div>
 			</div>
 			<div class="control-group">
@@ -26,35 +26,30 @@
 			<div class="control-group">
 				<label class="control-label">E-mail Address: *</label>
 				<div class="controls">
-					<input name="email" type="email" class="input-large" placeholder="x-wing@aliciousness.com">
+					<input name="email" type="email" class="input-xlarge">
 				</div>
 				<!--type e-mail incompatible with safari-->
 			</div>
 			<div class="control-group">
 				<label class="control-label">Phone Number:</label>
 				<div class="controls">
-					<input name="phone" type="tel" class="input-large" placeholder="(800)-400-2000">
+					<input name="phone" type="tel" class="input-xlarge">
 				</div>
 				<!-- type "tel" act same as "text" as of now.-->
 			</div>
 			<div class="control-group">
 				<label class="control-label">Zip-code:</label>
 				<div class="controls">
-					<input type="text" class="input-large" maxlength=5 placeholder="10080">
+					<input type="text" class="input-xlarge" maxlength=5>
 				</div>
 			</div>
 			<div class="control-group">
 				<label class="control-label">Language: (hold CTRL to select multiple options)</label>
 				<div class="controls">
-					<select multiple="multiple" class="input-large">
+					<select multiple="multiple" class="input-xlarge">
 						<option value="English">English</option>
-						<option value="Arabic">Arabic</option>
 						<option value="Cantonese">Cantonese</option>
-						<option value="French">French</option>
-						<option value="Japanese">Japanese</option>
-						<option value="Korean">Korean</option>
 						<option value="Mandarin">Mandarin</option>
-						<option value="Russian">Russian</option>
 						<option value="Spanish">Spanish</option>
 						<option value="Other">Other</option>
 					</select>
@@ -63,13 +58,13 @@
 			<div class="control-group">
 				<label class="control-label">Subject: *</label>
 				<div class="controls">
-					<input name="subject" type="text" class= "input-large" placeholder="Subject">
+					<input name="subject" type="text" class= "input-xlarge">
 				</div>
 			</div>
 			<div class="control-group">
 				<label class="control-label">Message: *</label>
 				<div class="controls">
-					<textarea name="message" rows=10 class="input-large" placeholder="Please summarize the issue..."></textarea>
+					<textarea name="message" rows=10 class="input-xlarge"></textarea>
 				</div>
 			</div>
 			<!-- a verification code needs to be inserted here-->
