0707421de873a982f5df300dc709edfab610c2d6
wxiao <wxiao@college.harvard.edu>
2013-07-31 13:46:01 -0400

    edited morefunctions

--- morefunctions.php
+++ morefunctions.php
@@ -9,29 +9,51 @@
 
     //This is a function that takes a string of emails and put it into one array
 	
-	function parseEmails($string) {
+	function parseEmailString($string) {
 		// first, tidy up the whitespaces.
 		$rawarray = str_replace(" ","",$string);
 		// then, clean up the contents outside of <...>
 		$mailarray = explode(",", $rawarray);
+
 		// then filter through the array for none-email formats and return the result array of mail addresses.
 		$goodmails = array();
+		$index = array(); 
+		
 		foreach($mailarray as $email){
-			$start = strpos($email,"<");
-			if ($start != FALSE) {
-				$end = strpos($email,">");
-				$email = substr($email,$start,$end-$start);
-				$email = str_replace("<","",$email);
-			}
-			if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-				array_push($goodmails, $email);
-			} else {
-				throw new Exception("Invalid email: " . $email); 
+			try { 
+				/* WSX COMMENT: almost exactly the same as your original 
+					function, except this erases duplicates */
+				$email = parseEmail($email); 
+				if(!isset($index[$email])) {
+					array_push($goodmails, $email); 
+					$index[$email] = ""; 
+				}	
+			} catch (Exception $e) {
+				throw new Exception("Function 'parseEmailString' failed with: [" . $e->getMessage() . "]"); 
 			}
 		}
 		return $goodmails;
 	}
-	
+
+	function parseEmail($email) {
+		$start = strpos($email,"<");
+		if ($start) { 
+			/* WSX COMMENT: saying $start != FALSE is the 
+				equivalent of saying $start == true, which is the equivalent 
+				of saying just "$start" */
+
+			$end = strpos($email,">");
+			$email = substr($email,$start,$end-$start);
+			$email = str_replace("<","",$email);
+		}
+		
+		if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+			return $email; 
+		} else {
+			throw new Exception("Function 'parseEmail' failed with: [Invalid email: " . $email . "]"); 
+		}
+	}
+		
 	$passing_strings = array(
 		"willy@chenxiao.us", 
 		"willy@chenxiao.us, tom@bob.com",
@@ -41,33 +63,34 @@
 		"wxiao@college.harvard.edu", 
 		"wxiao.debate@gmail.com", 
 		"willy@chenxiao.us, bob@tom.com, tom@bob.com", 
-		"willy@chenxiao.us, willy@chenxiao.us"
+		"willy@chenxiao.us, willy@chenxiao.us", 
+		"willy@chenxiao.us willy xiao <tom@bob.com>", 		
 	); 
 	$failing_strings = array(
 		"willy", 
 		"google.com", 
 		"@google.com", 
+		"<willy@chenxiao.us> <tom@bob.com>",
+		"<willy@chenxiao.us> willy xiao, <willy@chenxiao.us> <tom, willyxiao <willy@chenxiao.us>",
 		"willy@", 
 		"willy@chenxiao.us tom@bob.com", 
-		"willy@chenxiao.us willy xiao <tom@bob.com>", 
 		"willy@chenxiao.us, <willy xiao>, tom@bob.com", 
 		"willy@chenxiao.us, tom@bob.com, ble",
 		"willy@chenxiao.us, , tom@bob.com"
 	); 
-	
+
 	foreach($passing_strings as $str) {
 		echo "<p>".$str; 
-		var_dump(parseEmails($str)); 
+		var_dump(parseEmailString($str)); 
 		echo "</p>"; 
 	}
 	
 	foreach($failing_strings as $str) {
 		try {
-			echo "<p>Failed: "; 
-			var_dump(parseEmails($str));
-			echo "</p>"; 
+			var_dump(parseEmailString($str));
+			echo "<p>Failed to throw Error on " . $str . "</p>"; 
 		} catch (Exception $e) {
-			echo "<p>Successfully thrown error on : " . $str . "</p>"; 
+			echo "<p>Successfully thrown error on : " . $str . " " . $e->getMessage() . "</p>"; 
 		}
 	}
 	
