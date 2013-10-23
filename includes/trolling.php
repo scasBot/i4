0945d50ff13b8b81c91813d03813ba0746ad06be
wxiao <wxiao@college.harvard.edu>
2013-10-23 11:36:07 -0400

    Quote updated.

--- includes/trolling.php
+++ includes/trolling.php
@@ -4,20 +4,28 @@ class Filler {
 	private function random($arr, $seed_number = NULL) {
 		if(!$seed_number) {
 			return $arr[array_rand($arr)]; 
-		}
-		else {
+		} else {
 			return $arr[$seed_number % count($arr)]; 
 		}
 	}
 
-	public function random_quote($seed_number = NULL) {	
-		$quotes = query(query_select(array(
+	// seed_number not supported for now
+	public function random_quote($seed_number = NULL) {
+		try {
+			$quote = query("SELECT * FROM i3_Quotes ORDER BY RAND() LIMIT 1"); 
+			return $quote[0]["quote"]; 
+		} catch (Exception $e) {
+			return "\"Make each day your masterpiece.\" - John Wooden";
+		}
+		
+/*		$quotes = query(query_select(array(
 			"TABLE" => "i3_Quotes"))); 
 		$tmp = array(); 
 		foreach($quotes as $quote) {
 			$tmp[] = $quote["quote"]; 
 		}
 		return $this->random($tmp, $seed_number); 
+*/
 	}
 
 	private $celebs = array(
