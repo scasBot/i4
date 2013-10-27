c01fd8884df1e04a72ce5382712082d76074231d
wxiao <wxiao@college.harvard.edu>
2013-10-27 00:07:54 -0400

    Email validation for profile class

--- includes/profile_class.php
+++ includes/profile_class.php
@@ -4,7 +4,17 @@ class Profile extends aPureDataObject implements iDataObject {
 	protected $matchers = array("UserName", "Email", "YOG"); 
 	protected $elements = array("UserID", "UserName", "Email", "YOG", "Comper", "Hidden"); 
 	protected $primary_key = "UserID"; 
-	protected $database_name = "i3_Users"; 
+	protected $database_name = "i3_Users";
+
+	public function set($field, $val) {
+		if($field === "Email" 
+			&& !filter_var($val, FILTER_VALIDATE_EMAIL)) {
+			throw new Exception("Email address invalid."); 
+			return false; 
+		} else {
+			return parent::set($field, $val); 
+		}
+	}
 }
 
 class Password extends aPureDataObject implements iDataObject {
