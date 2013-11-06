503a1a7abbafba5b46f5c3dcd24aacbf6da6c3ae
wxiao <wxiao@college.harvard.edu>
2013-11-06 17:08:43 -0500

    Updated scripts for server

--- permissions.sh
+++ permissions.sh
@@ -1,3 +1,12 @@
+# all folders should not be globally accessible
+chmod 700 ajax/
+chmod 700 data/
+chmod 700 server/
+
+# html is publicly accessible, not readable
+chmod 711 html/ 
+
+# these files are globally readable
 chmod 744 html/js/*
 chmod 744 html/css/*
 chmod 744 html/img/*
---
+++ update.sh
@@ -0,0 +1,7 @@
+# for automatically updating scas server
+
+git checkout master
+git pull origin master
+
+git checkout beta
+git pull origin beta
