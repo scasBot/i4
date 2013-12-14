32885764e6a6a333410cd3e57f0adebeaf392860
scasBot <scas@hcs.harvard.edu>
2013-12-14 15:48:46 -0500

    server_config.php working now

--- server/server_config.php
+++ server/server_config.php
@@ -1,6 +1,11 @@
 <?php
+
+ini_set(display_errors, true); 
+error_reporting(E_ALL | E_STRICT); 
+
 define("CRON_ROOT", "/nfs/home/groups/scas/web/i4/"); 
 require(CRON_ROOT . "includes/constants.php"); 
+require(CRON_ROOT . "includes/constants_passwords.php"); 
 require(CRON_ROOT . "includes/libraries/ALL.php"); 
 require(CRON_ROOT . "includes/functions_database.php"); 
 ?>
