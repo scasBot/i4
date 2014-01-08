de60e2678b6131818dd1c7307442e923b392b967
scasBot <scas@hcs.harvard.edu>
2014-01-08 11:56:59 -0500

    Fixed permissions for the new api

--- server/permissions.sh
+++ server/permissions.sh
@@ -15,11 +15,13 @@ chmod 700 templates/*
 # html is publicly accessible, not readable
 chmod 711 html/ 
 chmod 711 html/*.php
+chmod 711 html/api/*.php
 
 # all these directories publicly accessible, not readable
 chmod 711 html/js/
 chmod 711 html/css/
 chmod 711 html/img/
+chmod 711 html/api/
 
 # these files are globally readable
 chmod 644 html/js/*.js
