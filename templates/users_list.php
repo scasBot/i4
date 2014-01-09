d2989a9c2821efbe9b8550230705645d77f84961
Keon Chris Lim <keon.chris.lim@gmail.com>
2014-01-08 19:49:00 -0500

    minor visual enhancements to users_list

--- templates/users_list.php
+++ templates/users_list.php
@@ -1,26 +1,23 @@
-<div id="find">
-	<section class="bottom">
-		<div class="row">
-			<table class="table table-bordered table-hover">
-				<thead>
-					<tr>
-						<th>Name</th>
-						<th>Email</th>
-						<th>Graduation Year</th>
-					</tr>
-				</thead>
-				<tbody>
-					<?php foreach($users as $user) : ?>
-					<tr class="user" onclick="goto(<?php echo $user["UserID"] ?>);" >
-						<td><?php echo $user["UserName"] ?></td>
-						<td><?php echo $user["Email"] ?></td>
-						<td><?php echo $user["YOG"] ?></td>
-					</tr>
-					<?php endforeach; ?>
-				</tbody>
-			</table>
-		</div>
-	</section>
+<div class="profile-wrapper">
+	<h1>List of Users</h1>
+	<table class="table table-bordered table-hover" style="width: 80%; cursor: pointer">
+		<thead>
+			<tr>
+				<th>Name</th>
+				<th>Email</th>
+				<th>Graduation Year</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php foreach($users as $user) : ?>
+			<tr class="user" onclick="goto(<?php echo $user["UserID"] ?>);" >
+				<td><?php echo $user["UserName"] ?></td>
+				<td><?php echo $user["Email"] ?></td>
+				<td><?php echo $user["YOG"] ?></td>
+			</tr>
+			<?php endforeach; ?>
+		</tbody>
+	</table>
 </div>
 <script>
 	function goto(id) {
