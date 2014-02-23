ab756ea0879fb914523b6edc43042d7beb8755eb
wxiao <wxiao@college.harvard.edu>
2014-02-22 23:45:09 -0500

    Stats by i4 round 1

--- templates/profile_stats_form.php
+++ templates/profile_stats_form.php
@@ -1,7 +1,11 @@
 <div class="row">
 	<div class="span3"></div>
 	<div class="span6">
-		<div id="chart_div"></div>
+		<h1><?php echo byi4("Stats") ?></h1>
+		<div>
+			<div id="chart_div" style="margin: 0 auto; display: inline-block" ></div>
+		</div>
+		<div id="chart_month_div"></div>
 		<script type="text/javascript" src="https://www.google.com/jsapi"></script>
 		<script type="text/javascript">
 		  // Load the Visualization API and the piechart package.
@@ -34,7 +38,27 @@
 			// Instantiate and draw our chart, passing in some options.
 			var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
 			chart.draw(data, options);
-		  }
+			
+			var data_bar = new google.visualization.arrayToDataTable([
+			  ['Month', 'Contacts'],
+			  <?php
+				$month = array(1 => "Jan", 2 => "Feb", 3 => "Mar", 4 => "Apr", 5 => "May", 
+					6 => "Jun", 7 => "Jul",8 => "Aug", 9 => "Sep", 10 => "Oct", 11 => "Nov", 12 => "Dec"
+				)
+			  ?>
+			  <?php foreach($stats["clients_by_month"] as $mon): ?>
+				['<?php echo $month[$mon["month"]] ?>', <?php echo $mon["clients"] ?>], 
+			  <?php endforeach; ?>
+			]);
+
+			var options_bar = {
+			  title: 'Assisted by Month',
+			  hAxis: {title: 'Month', titleTextStyle: {color: 'black'}}
+			};
+
+			var chart = new google.visualization.ColumnChart(document.getElementById('chart_month_div'));
+			chart.draw(data_bar, options_bar);
+		}
 		</script>	
 	</div>
 	<div class="span3"></div>
