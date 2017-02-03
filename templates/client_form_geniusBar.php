3e1b58bbb4ccafab9edcdb6c28c44667239fb1f0
Elton Garret Lossner <eltonlossner@college.harvard.edu>
2017-02-03 10:08:29 +0000

    Hide all email buttons due to being nonfunctional

--- templates/client_form_geniusBar.php
+++ templates/client_form_geniusBar.php
@@ -1,9 +1,9 @@
-	<button class="btn btn-default actions" style="height: 50px;" data-action="emaili4">Email i4 Users</button>
-    <!--<button class="btn btn-default actions" style="height: 50px; ;" data-action="emaili4">Email i4 Users</button> -->
+	<!-- <button class="btn btn-default actions" style="height: 50px;" data-action="emaili4">Email i4 Users</button>
+    <button class="btn btn-default actions" style="height: 50px; ;" data-action="emaili4">Email i4 Users</button>
     <button class="btn btn-primary actions" style="height: 50px;" data-action="emailLegalResearch">Email Legal Research</button>
-    <!--<button class="btn btn-primary actions" style="height: 50px; ;" data-action="emailLegalResearch">Email Legal Research</button> -->
+    <button class="btn btn-primary actions" style="height: 50px; ;" data-action="emailLegalResearch">Email Legal Research</button>
 	<button class="btn btn-success actions" style="height: 100px;" data-action="emailClient"><i class="glyphicon glyphicon-envelope"></i> Email Client</button>
-	<!-- button class="btn btn-inverse actions" data-action="email">Email Client</button -->
+	<button class="btn btn-inverse actions" data-action="email">Email Client</button --> 
 <script>
 $(document).ready(function() {
 	$(".actions").on("click", function() {
