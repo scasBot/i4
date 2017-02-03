622afd9f987c4a2c6ec6a59bdf398bc94608a872
Elton Garret Lossner <eltonlossner@college.harvard.edu>
2017-02-03 09:55:58 +0000

    LR button update

--- templates/client_form_geniusBar.php
+++ templates/client_form_geniusBar.php
@@ -67,6 +67,7 @@ $(document).ready(function() {
 		emailLegalResearch : function() {
 			addEmailHandler(function(emailForm) {
 				emailForm.from(constants.userEmail); 
+				emailForm.senderName(constants.userName);
 				emailForm.to("masmallclaims@gmail.com"); //constants.legalResearchEmail - old
 				emailForm.inputFieldObj("to").prop("disabled", true); 
 				emailForm.subject("SCAS Referral: Client " + constants.clientId); 
