37f542a3b3723968e052370817b03965fa189730
ifeomi <omidiran@college.harvard.edu>
2020-08-09 21:39:12 -0500

    Create prototype for sending and receiving SMS with Twilio

---
+++ html/reply_sms.php
@@ -0,0 +1,11 @@
+<?php
+// Include the bundled autoload from the Twilio PHP Helper Library
+require dirname(__DIR__) . '/vendor/twilio-php-main/src/Twilio/autoload.php';
+use Twilio\TwiML\MessagingResponse;
+// Set the content-type to XML to send back TwiML from the PHP Helper Library
+header("content-type: text/xml");
+$response = new MessagingResponse();
+$response->message(
+    "I'm using the Twilio PHP library to respond to this SMS!"
+);
+echo $response;
