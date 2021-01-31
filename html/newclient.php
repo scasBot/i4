0439a1d01d35319e0e5b5515bc8720615d45933b
ifeomi <omidiran@college.harvard.edu>
2021-01-31 18:01:12 -0500

    Set additional default values for new clients to fix issue with insert queries

--- html/newclient.php
+++ html/newclient.php
@@ -9,45 +9,52 @@ Developed for SCAS i4
 masmallclaims@gmail.com
 
 To use code, please contact SCAS or
-Willy at the above emails. 
+Willy at the above emails.
 
 August 2013
 
-Description : 
-	Adds a new client to the database, 
+Description :
+	Adds a new client to the database,
 	$_POST variables must be extremely robust and match
 	all the entries of the class ClientInfo
 ***********************************/
-require("../includes/config.php"); 
-require("../includes/client_class.php"); 
+require("../includes/config.php");
+require("../includes/client_class.php");
 
 // accessing by GET is incorrect, sorry
 if($_SERVER["REQUEST_METHOD"] == "GET") {
-	apologize("Can't access this page by GET request."); 
+	apologize("Can't access this page by GET request.");
 }
 
 // accessing by POST is correct
 else if($_SERVER["REQUEST_METHOD"] == "POST") {
 
 	// create the new client info entry in the database
-	$info = new ClientInfo(); 
-	assert2($info->from_array($_POST), "POST items are incorrect." );
-	assert2($info->set("State", "MA"));  // default state is MA
-	assert2($info->set("Language", "English")); // default language is English
-	assert2($info->set("CaseTypeID", 21)); 
-	assert2($info->push(), "Failed to insert into server in newclient.php"); 
-	$client_id = $info->get("ClientID"); 
-	
+	$info = new ClientInfo();
+	assert2($info->from_array($_POST), "POST items are incorrect.");
+
+	// set default values
+	assert2($info->set("State", "MA"));
+	assert2($info->set("Language", "English"));
+	assert2($info->set("ClientID", 0));
+	assert2($info->set("CategoryID", 0));
+	assert2($info->set("CourtDate", NULL));
+	assert2($info->set("CaseTypeID", 21));
+	assert2($info->set("ReferralSpecify", ""));
+
+	assert2($info->push(), "Failed to insert into server in newclient.php");
+	$client_id = $info->get("ClientID");
+
 	// if there's an emailId, add it as a new contact
 	if (isset($_POST["emailId"]))
 	{
 		redirect("assignEmail.php?ClientID=" . $client_id . "&id=" . $_POST["emailId"]);
-	}	
+	}
 	else
 	{
 		// redirects to the client's contact page
-		redirect("client.php?ClientID=" . $client_id); 
+		redirect("client.php?ClientID=" . $client_id);
 	}
 }
-	
+
 ?>
