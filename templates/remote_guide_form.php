f25922b2e708a5009ace4c92404e5468f2ee149e
genehlee <genel0625@gmail.com>
2021-03-02 15:57:37 -0500

    Update remote office hours guide (#73)

--- templates/remote_guide_form.php
+++ templates/remote_guide_form.php
@@ -1,6 +1,7 @@
 <div class="profile-wrapper">
-    <h1>Fall 2020 Office Information</h1>
+    <h1>Spring 2021 Office Information</h1>
+    <p>Make sure to <a href="https://docs.google.com/forms/d/e/1FAIpQLSd-bXwWiR6GfMlM-4cDXK1eqSSsYMGmjrAKKmkMzlmKBY7qag/viewform">sign out</a>!</p>
     <iframe class="embed" height="640px" width="800px" src=
-        "https://docs.google.com/document/d/e/2PACX-1vQxDywb3LrL4isCruHtwoKoDi8UvkgEEkzB2TGHzA0SkZoPzZmP3uv32pjtc3PhSD1BA2llBbb2PXAu/pub?embedded=true">
+        "https://docs.google.com/document/d/e/2PACX-1vTrAJrPU_tvOd4p_Ohx03zJDt6tvY1VmnSCL4cNW46Kerpb2EVWydvtGo30VvvGr-AzGIz8Djy0IhaT/pub?embedded=true">
     </iframe>
 </div>
