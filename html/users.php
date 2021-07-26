da0a9b5ce3b8683aeed57bc572e94d161be5a226
ifeomi <omidiran@college.harvard.edu>
2021-07-26 11:22:43 -0400

    remove trailing whitespace and unneeded echo

--- html/users.php
+++ html/users.php
@@ -9,30 +9,29 @@ Developed for SCAS i4
 masmallclaims@gmail.com
 
 To use code, please contact SCAS or
-Willy at the above emails. 
+Willy at the above emails.
 
 January 2014
 
 Description : Displays all the users of the i4
 ***********************************/
-require("../includes/config.php"); 
+require("../includes/config.php");
 
 switch ($_SERVER["REQUEST_METHOD"]) {
-	case "POST" : 
-		apologize("Can't access that way yet."); 
-		break; 
-	case "GET" : 
+	case "POST":
+		apologize("Can't access that way yet.");
+		break;
+	case "GET":
 		$query = "SELECT UserID, UserName, Email, YOG "
 			. "FROM i3_Users "
 			. "WHERE Hidden=0 "
-			. "ORDER BY YOG ASC, UserName ASC " ; 
-		echo $query; 
+			. "ORDER BY YOG ASC, UserName ASC " ;
 		$users = query($query);
-		render("users_list.php", array("title" => "i4 Users", 
+		render("users_list.php", array("title" => "i4 Users",
 			"users" => $users));
 		break;
-	default: 
-		apologize("Wrong access type."); 
-		break; 
-}	
+	default:
+		apologize("Wrong access type.");
+		break;
+}
 ?>
